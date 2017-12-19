0d378ceebf9aa6f9acb3ace8763c46886bdf6530
Drew André <drewjamesandre@gmail.com>
2017-12-18 21:47:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Ripple
+
+[ ![Codeship Status for drewandre/ripple](https://app.codeship.com/projects/170c8390-bb87-0135-37fd-46dcf572bfca/status?branch=master)](https://app.codeship.com/projects/259373)
+
+## To run locally:
+* `git clone https://gitlab.com/drewandre/ripple.git`
+* `bundle install`
+* `foreman start`, and visit `localhost:5000`
+* to run react tests, `karma start` and open localhost:9397
+* use `rspec` for unit tests
---
+++ spec/factories/users.rb
@@ -0,0 +1,20 @@
+require "factory_girl"
+
+FactoryGirl.define do
+  factory :user do
+    confirmed_at Time.current
+    sequence(:email) { |n| "user#{n}@example.com" }
+    sequence(:first_name) { |n| "Thor#{n}" }
+    sequence(:handle) { |n| "user#{n}" }
+    sequence(:last_name) { |n| "Andre#{n}" }
+    sequence(:universally_unique_id) { |n| "Alpha#{n}Numeric" }
+    password "password"
+    password_confirmation "password"
+
+    trait :unconfirmed do
+      confirmed_at nil
+    end
+
+    factory :unconfirmed_user, traits: [:unconfirmed]
+  end
+end
