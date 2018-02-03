73b05edd05bc2a48c13356a9d57b84ac3ffb335d
Drew André <drewjamesandre@gmail.com>
2018-02-03 17:25:25 -0500

    build out user, serializer, and session unit tests

--- app/javascript/react/src/containers/App.js
+++ app/javascript/react/src/containers/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import Dashboard from './DashboardViews/Dashboard';
-
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 function mapStateToProps(state) {
 	return {
-		currentUser: state.currentUser.user
+		currentUser: state.currentUser
 	};
 }
 
 @connect(mapStateToProps)
-class App extends Component {
+export class App extends Component {
 	render() {
 		return <Dashboard />;
 	}
