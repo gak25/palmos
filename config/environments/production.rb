2e503f9166968e46227825e9c0ee1951a2f28e79
Drew André <drewjamesandre@gmail.com>
2018-02-18 15:32:40 -0500

    expose ENV variables for actionmailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,16 +62,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: "#{Rails.application.class.parent_name.downcase}.#{Rails.application.secrets.secret_gmail_domain}" }
-    config.action_mailer.delivery_method = :sendmail
+    config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       :address        => "smtp.gmail.com",
       :port           => 587,
-      :domain         => "#{Rails.application.secrets.secret_gmail_domain}",
+      # :domain         => "#{Rails.application.class.parent_name.downcase}.#{Rails.application.secrets.secret_gmail_domain}",
       :authentication => :plain,
-      :user_name      => "#{Rails.application.secrets.secret_gmail_username}",
-      :password       => "#{Rails.application.secrets.secret_gmail_password}",
+      # :user_name      => "#{Rails.application.secrets.secret_gmail_username}",
+      # :password       => "#{Rails.application.secrets.secret_gmail_password}",
+      :domain         =>  "palmos.co",
+      :user_name      =>  ENV["SECRET_GMAIL_USERNAME"],
+      :password       =>  ENV["SECRET_GMAIL_PASSWORD"],
       :enable_starttls_auto => true
     }
     config.action_mailer.perform_caching = false
