b19fc4632d428f7a782381866b6c104e65e568cd
Drew André <drewjamesandre@gmail.com>
2018-02-18 15:55:08 -0500

    remove stray character from baseUrl.js

--- app/javascript/react/src/constants/baseUrl.js
+++ app/javascript/react/src/constants/baseUrl.js
@@ -2,7 +2,7 @@ let baseUrl;
 
 switch (process.env.NODE_ENV) {
 	case 'production':
-		baseUrl = 'https://www.palmos.co/';
+		baseUrl = 'https://www.palmos.co';
 		break;
 	default:
 		baseUrl = 'http://localhost:3000';
