e08bbc8d9fdf454fc43bb08f707f6204a9b45065
Drew André <drewjamesandre@gmail.com>
2018-02-18 15:43:30 -0500

    remove dotenv production requirement in favor of heroku config settings

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,7 @@
-require "dotenv-rails"
-Dotenv.load(".env")
+if Rails.env === "development" || Rails.env === "test"
+  require "dotenv-rails"
+  Dotenv.load(".env")
+end
 
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
